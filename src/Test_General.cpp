5418e51c6afc97a4d25d8ed058f779fdc7905fb7
GodPlex <keszler@college.harvard.edu>
2018-03-22 21:26:48 -0400

    misc changes

--- src/Test_General.cpp
+++ src/Test_General.cpp
@@ -180,6 +180,7 @@ void  TestIt(long R, long p, long r, long d, long c, long k, long w,
      if (!noPrint) CheckCtxt(c2, buffer);
      debugCompare(ea,secretKey,p2,c2);
 
+     
      ::negate(ea, p1); // c1.negate()
      c1.negate();
      if (!noPrint) CheckCtxt(c1, "c1=-c1");
